a0a55552b4fc64e172f9dc7650a81b66eed76876
Lilita1762 <lilibeth.tabares@gmail.com>
2020-06-11 00:50:17 +0000

    First C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,11 @@
 /**
  * main - entry point
  *
- * return always 0
-*/
+ * Return: always 0
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
