68b9dc3e4d01af7bae7b2b417260a18b8e74867f
Lilita1762 <lilibeth.tabares@gmail.com>
2020-06-12 01:38:05 +0000

    Prints all single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0' ; ch <= '9' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar ('\n');
+	return (0);
+}
