67399273883990b6ccd2bc3e7402bc0dac646a99
Lilita1762 <lilibeth.tabares@gmail.com>
2020-06-16 20:24:41 +0000

    Checks lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,16 +5,16 @@
  *
  * @c: Value of input
  *
- * Return 1 or 0
+ * Return: 1 or 0
  */
 int _islower(int c)
 {
-if (c >= 'a' && c <= 'z')
-
-return (1);
-
-else
-
-return (0);
-
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
