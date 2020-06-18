42a5ae184c3a1421705c38ea498c5efa2a119daa
Lilita1762 <lilibeth.tabares@gmail.com>
2020-06-18 23:19:14 +0000

    Prints the numbers from 0 to 9 and a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* print_numbers - prints the numbers
+*
+*
+* Return: 0.
+*/
+void print_numbers(void)
+{
+	int i;
+
+for (i = 48; i < 58; i++)
+{
+	_putchar(i);
+}
+{
+	_putchar('\n');
+}
+}
