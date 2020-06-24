dc11e55aadfb4361f11c4e3d7dae6c3aa773e064
Lilita1762 <lilibeth.tabares@gmail.com>
2020-06-24 02:18:25 +0000

    Copies the string pointed to by src, to by dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,9 +11,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int c;
 
-	for (c = 0; src[c] !=0; c++)
+	for (c = 0; src[c] != 0; c++)
 		dest[c] = src[c];
 	dest[c] = 0;
 
-	return(dest);
+	return (dest);
 }
