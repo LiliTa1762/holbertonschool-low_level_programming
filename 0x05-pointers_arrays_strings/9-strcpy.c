2b482917a3701e5020daa23fd2afe4ed47d22982
Lilita1762 <lilibeth.tabares@gmail.com>
2020-06-24 02:16:59 +0000

    Copies the string pointed to by src, to by dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ **_strcpy - string pointed to src to the buffer pointed
+ *
+ *@dest: buffer
+ *@src: pointed to by
+ *
+ *Return: copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int c;
+
+	for (c = 0; src[c] !=0; c++)
+		dest[c] = src[c];
+	dest[c] = 0;
+
+	return(dest);
+}
