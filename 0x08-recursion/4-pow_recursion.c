83538bf0eef6349a4609d20dbd40bc07567adfcb
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-02 00:49:01 +0000

    Returns the value of x raised to the power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *_pow_recursion - raise to the power of a variable
+ *@x: variable
+ *@y: variable
+ *Return: 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y > 0)
+	{
+		return (x * _pow_recursion (x, y - 1));
+	}
+
+	return (x);
+}
