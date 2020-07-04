897633f62d6353db6bf6ae35c41488ecb7f4ebde
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-04 20:23:04 +0000

    Prints the number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *main - function
+ *@argc: argument counter
+ *@argv: argument vector
+ *Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf ("%d\n", argc --);
+		return (0);
+}
