4477effd356cdbadbf8d0452e2a3373fc532c951
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-05 02:48:34 +0000

    Adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "holberton.h"
+/**
+ *main - function
+ *@argc: counter
+ *@argv: vector
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int count, count2;
+	int total = 0;
+
+	if(argc > 1)
+	{
+		for (count = 1; argv[count]; count++)
+		{
+			for (count2 = 0; argv[count][count2]; count2++)
+			{
+				if (!isdigit(argv[count][count2]))
+				{
+					puts("Error");
+					return (1);
+				}
+			}
+			total += atoi(argv[count]);
+		}
+	}
+	printf("%d\n", total);
+
+	return (0);
+}
