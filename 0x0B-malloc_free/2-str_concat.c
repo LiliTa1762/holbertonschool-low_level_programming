5c33ce65709ae7c157e9ef45d3283497735e96a8
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-09 21:39:27 +0000

    Function that concatenates two strigs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,20 +23,18 @@ char *str_concat(char *s1, char *s2)
 
 	for (counter = 0; s1[counter] != '\0'; counter++)
 		;
-	for (counter2 = 0; s2[counter] != '\0'; counter2++)
+	for (counter2 = 0; s2[counter2] != '\0'; counter2++)
 		;
 	a = malloc(((sizeof(*s1) * counter) + (sizeof(*s2) * counter2)) + 1);
 	{
 		if (a == NULL)
-		return (NULL);
+			return (NULL);
 	}
 	for (c = 0, c2 = 0; c < (counter + counter2 + 1); c++)
 		;
-	{
-		if (c < counter)
-			a[c] = s1[c];
-		else
-			a[c] = s2[c2++];
-	}
+	if (c < counter)
+		a[c] = s1[c];
+	else
+		a[c] = s2[c2++];
 	return (a);
 }
