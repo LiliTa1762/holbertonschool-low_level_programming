6b08267ca02fe7878a917af23bb217e0f1756295
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-10 01:18:10 +0000

    Allocate memory for an array with malloc and calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ *_calloc - calloc
+ *@nmemb: variable
+ *@size: variable
+ *Return: null
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+}
