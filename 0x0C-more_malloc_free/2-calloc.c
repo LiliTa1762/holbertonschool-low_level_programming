acaec5bdf29c37443a8d555731d74aa2ff8936bb
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-11 15:56:31 +0000

    Function that allocates memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *a;
 	unsigned int i;
 
-	if (nmemb == 0 && size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	a = malloc(nmemb * size);
