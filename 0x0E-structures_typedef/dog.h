c0f7761c50d1dd4fdc540b51506e6c8ceee11596
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-14 04:41:42 +0000

    Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,15 +6,17 @@
  *@age: variable
  *@owner: pointer
 */
-struct dog
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 
 #endif
