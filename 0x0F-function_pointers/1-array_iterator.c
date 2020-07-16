5abad61d44bb220f5f79eaa4262778f15cb76038
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-16 03:06:14 +0000

    Executes a function given as a parameter on each element

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,6 +16,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 		return;
 
 	if (action != NULL)
-		for(i = 0; i <= size; i++)
+		for (i = 0; i < size; i++)
 			action(array[i]);
 }
