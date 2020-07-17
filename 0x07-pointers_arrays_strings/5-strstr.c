26de29ee6b74a33fc7342f081556d39aadceabe4
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-17 01:08:21 +0000

    Function that locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+int _cmp(char *s1, char *s2)
+{
+	for (; *s1 && *s2; s1++, s2++)
+		if (*s1 != *s2)
+			return (0);
+
+	return (*s2 == '\0');
+}
+
+/**
+ *_strstr - function
+ *@haystack: pointer
+ *@needle: pointer
+ *Return: pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if(_cmp(&haystack[i], needle))
+		{
+			return (&haystack[i]);
+		}
+	}
+	if (haystack[i] != *needle)
+		return (0);
+
+	return (&haystack[i]);
+}
