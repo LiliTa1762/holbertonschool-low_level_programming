62611cedd533775173bac90c259b4e05f02e585c
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-18 20:54:43 +0000

    Prints numbers with separator and printf

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(nlist, n);
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(nlist, int));
 		if (i < (n - 1))
