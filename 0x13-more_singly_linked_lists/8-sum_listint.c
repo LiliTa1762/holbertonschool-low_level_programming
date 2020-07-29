24f3527115e702175817c33cd2b320f49ca11a08
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-29 04:34:57 +0000

    Returns the sum of all data(n)

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -6,10 +6,15 @@
  */
 int sum_listint(listint_t *head)
 {
-	int sum;
+	int sum = 0;
 
 	if (head == NULL)
 		return(0);
 
+	while (head)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
 	return (sum);
 }
