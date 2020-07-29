eaeea6304882cbeebfe1ea61d805af6b6446f403
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-29 04:16:23 +0000

    Returns the nth node of a list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *get_nodeint_at_index - function to get node
+ *@head: variable
+ *@index: index of the node
+ *Return: NULL or nth node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *nnode;
+
+	if (head == NULL)
+		return(NULL);
+
+
+	return(nnode);
+}
