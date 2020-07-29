17e1c1c4de4c4e70d367e4ae275629d952b7eece
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-29 04:58:15 +0000

    Returns nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,11 +7,23 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *nnode;
+	listint_t *nnode = head;
+	listint_t *f = head;
+	unsigned int count = 1;
 
 	if (head == NULL)
-		return(NULL);
+		return (NULL);
 
+	while (count <= index - 1)
+	{
+		f = f->next;
+		count = count + 1;
+	}
 
-	return(nnode);
+	while (f->next == NULL)
+	{
+		f = f->next;
+		nnode = nnode->next;
+	}
+	return (nnode);
 }
