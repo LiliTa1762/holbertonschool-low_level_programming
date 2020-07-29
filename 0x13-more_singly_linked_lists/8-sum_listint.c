8145ba24f52c4e18fdd03d5cd508dddc99b5db48
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-29 04:20:43 +0000

    Returns the sum of all data

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ *sum_listint - function to sum
+ *@head: pointer to list
+ *Return: int or 0
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	if (head == NULL)
+		return(0);
+
+	return (sum);
+}
