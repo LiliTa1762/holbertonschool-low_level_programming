e2fbdfc5853a88b8adf2fb03e2f66f37fb170907
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-31 02:16:07 +0000

    Returns the value of a bit in a index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *get_bit - function to get a bit in an index
+ *@n: variable
+ *@index: given index
+ *Return: value of a bit or -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+
+	return (-1);
+}
