9dab87dbb757fb9bad9dfb27f7e48d289c161a51
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-31 02:24:22 +0000

    Function that sets the value to 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *set_bit - function to set the value of a bit to 1
+ *@n: pointer
+ *@index: given index
+ *Return: 1 or -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+	return (-1);
+}
