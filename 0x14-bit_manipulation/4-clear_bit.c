a09ce4bd95bfbe2c444dcbb3789b9e35e7336e38
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-31 02:24:57 +0000

    Sets the value to 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *clear_bit - funtions to sets the value to 0
+ *@n: pointer
+ *@index: given index
+ *Return: 1 or -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+	return (-1);
+}
