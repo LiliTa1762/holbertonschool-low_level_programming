a272ca086cdae6bb4987dd0a00a419f8e436d20b
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-31 03:03:33 +0000

    Returns the value of a bit in an index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,6 +7,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-
-	return (-1);
+	if (index > 63)
+		return (-1);
+	if (index == 0)
+	{
+		return (n & 1);
+	}
+	return (get_bit(n >> 1, index - 1));
 }
