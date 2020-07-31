d58e53a7f186179df1e6bcc94595de9c0b552d05
Lilita1762 <lilibeth.tabares@gmail.com>
2020-07-31 02:26:18 +0000

    Flip to get a number

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *flip_bits - funtion to flip
+ *@n: variable
+ *@m: variable
+ *Return: the number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+	return (n);
+}
