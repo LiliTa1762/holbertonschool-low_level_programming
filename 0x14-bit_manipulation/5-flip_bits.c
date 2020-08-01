999b0c898b2a737283aff2a1d9fd41ba8356bf49
Lilita1762 <lilibeth.tabares@gmail.com>
2020-08-01 00:04:25 +0000

    Return the number of bits, needed to flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,6 +7,15 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int f;
+	int i = 0;
 
-	return (n);
+	f = n ^ m;
+
+	while (f)
+	{
+		i += f & 1;
+		f >>= 1;
+	}
+	return (i);
 }
