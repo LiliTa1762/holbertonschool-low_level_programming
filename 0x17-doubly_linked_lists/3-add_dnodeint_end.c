cd84fa6918f99e9beaeddcf70f8b9bf8526a5033
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-04 21:27:28 +0000

    Add a node at the end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *add_dnodeint_end - Function to add a new node at the end of a list
+ *@head: pointer to head
+ *@n: int
+ *Return: the address of new element, or NULL
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *last;
+
+	new = malloc(sizeof(dlistint_t));
+	{
+		if (new == NULL)
+			return (NULL);
+	}
+	last = (*head);
+
+	new->n = n;
+
+/* Position: The new node is gonna be the last, so the next of it is NULL */
+	new->next = NULL;
+
+/* If the linked list is empty, then the new node is head */
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		(*head) = new;
+		return (0);
+	}
+
+/* If it is not empty, traverse till the last node */
+	while (last->next != NULL)
+		last = last->next;
+
+/* Now, change the next of last */
+	last->next = new;
+
+/* Finally, the last node is the prev of the new node */
+	new->prev = last;
+
+	return (new);
+}
