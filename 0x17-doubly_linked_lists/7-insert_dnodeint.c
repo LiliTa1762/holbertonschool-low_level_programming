830b36d407776f804d41c082a33ef4bea091a0b3
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-06 04:18:25 +0000

    Inserts a new node at a x position. Awfull spaces

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,44 +20,35 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
 	new->next = NULL;
-/* The node has to be assign in idx as head, and head is null */
 	if ((*h) == NULL)
 	{
 		if (idx != 0)
+		{
 			return (0);
+		}
 		else
 			(*h) = new;
 	}
-
-/* The node has to be assign in idx as head, but head is not null */
 	if ((*h) != NULL && idx == 0)
 	{
 		new->next = (*h);
 		(*h) = new;
 		return (0);
 	}
-
 	current = (*h);
 	current->prev = NULL;
-
-/* Assign new node to the idx found or break if not found */
 	while (count < idx)
 	{
 		current = post;
 		post = post->next;
-
 		if (current == NULL)
-		{
 			break;
-		}
 		count++;
 	}
 	current->next = new;
 	post->prev = new;
 	new->next = post;
-
 	return (new);
 }
