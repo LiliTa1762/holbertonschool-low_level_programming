e9d0d6eb69cd0ae14b569ab46b3dc79aa091506d
Lilita1762 <lilibeth.tabares@gmail.com>
2020-09-06 04:55:52 +0000

    Inserts a new node at a x position. Fixed condition

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -44,7 +44,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = post;
 		post = post->next;
 		if (current == NULL)
-			break;
+			return (NULL);
 		count++;
 	}
 	current->next = new;
