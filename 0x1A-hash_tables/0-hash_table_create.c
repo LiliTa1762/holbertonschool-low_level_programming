9d5de68f9a10e8557ba661cbee6434b314d63055
Lilita1762 <lilibeth.tabares@gmail.com>
2020-10-02 16:42:55 +0000

    Create a HT

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,13 +15,24 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i = 0;
 
 	hash_table = malloc(sizeof(hash_table_t));
+	{
+		if (hash_table == NULL)
+			 return (NULL);
+	}
+
+	hash_table->size = size;
 
 	hash_table->array = malloc(sizeof(hash_node_t) * size);
+	if (hash_table->array == NULL)
+	{
+		free (hash_table);
+		return (NULL);
+	}
 
 	for (; i < size; i++)
 	{
 		hash_table->array[i] = NULL;
 	}
 
-	return hash_table;
+	return (hash_table);
 }
