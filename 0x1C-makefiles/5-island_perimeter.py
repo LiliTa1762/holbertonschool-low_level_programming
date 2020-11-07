c231063fc6fd07f3510446d49a3568abe53ad3c1
Lilita1762 <lilibeth.tabares@gmail.com>
2020-11-07 04:24:55 +0000

    Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+
+R = 5
+C = 6
+
+
+# Find the number of covered side for grid[i][j].
+def num_of_sides(grid, i, j):
+
+    count = 0
+
+    # Going up
+    if (i > 0 and grid[i - 1][j]):
+        count += 1
+
+    # Going left
+    if (j > 0 and grid[i][j - 1]):
+        count += 1
+
+    # Going down
+    if (i < R - 1 and grid[i + 1][j]):
+        count += 1
+
+    # Going Right
+    if (j < C - 1 and grid[i][j + 1]):
+        count += 1
+
+    return count
+
+
+# Returns sum of perimeter of shapes formed with "1"
+def island_perimeter(grid):
+
+    perimeter = 0
+
+    # Traversing the grid and finding "1"
+    # calculate their contribution.
+    for i in range(0, R):
+        for j in range(0, C):
+            if (grid[i][j]):
+                perimeter += (4 - num_of_sides(grid, i, j))
+
+    return perimeter
